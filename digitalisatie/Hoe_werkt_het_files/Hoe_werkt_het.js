b17b2eea518ccf9dae1ea4ccc428861a09b9bd70
robni7 <45372858+robni7@users.noreply.github.com>
2024-05-13 01:05:43 +0200

    1st part of thipom site

---
+++ digitalisatie/Hoe_werkt_het_files/Hoe_werkt_het.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20230805
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Hoe_werkt_het_files/Hoe_werkt_hetMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
